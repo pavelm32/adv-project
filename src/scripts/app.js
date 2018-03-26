a4e182c0a9242e04a7be3c7a25df714a4b72f5c3
PAVEL MOSKOVETS <>
2018-03-26 23:27:53 +0300

    main page gsl

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,7 @@
 const plugin = require('./common/test');
+const water = require('./index-water');
+
 
 plugin();
+
+water();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack'),
+      UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+      path = require("path");
 
-const config = {
+module.exports = {
     output: {
         filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                // query: {
+                //     presets: ['es2015']
+                // }
+            },
+            {
+                test: /\.(frag|vert)$/,
+                loader: 'webpack-glsl-loader'
+            }
+        ]
+        /*loaders: [
+            {
+                test: /\.(frag|vert)$/,
+                loader: 'webpack-glsl'
+            }
+        ]*/
     },
     plugins: [
         new UglifyJSPlugin({
             sourceMap: true
         })
-    ]
+    ],
 };
-
-module.exports = config;
